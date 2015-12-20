3abe9a3349d5fde462149b3cea6b3556e797c7d8
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2015-12-20 09:09:32 +0200

    Rename file

--- topology.py
+++ topology.py
@@ -5,7 +5,7 @@ from mininet.cli import CLI
 
 tree4 = TreeTopo(depth=2,fanout=2)
 net = Mininet(topo=tree4, controller=None)
-net.addController('firewall', controller=RemoteController, ip="127.0.0.1",
+net.addController('poxController', controller=RemoteController, ip="127.0.0.1",
     port=6633)
 net.start()
 CLI(net)
